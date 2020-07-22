2526845ed294a51ed34e41312e80e85da27c9393
LenaSim <radical.rin@gmail.com>
2020-07-22 21:15:31 +0200

    Add project card and styling

--- front/src/Components/Portfolio.js
+++ front/src/Components/Portfolio.js
@@ -1,39 +1,22 @@
 import React from 'react'
 import axios from 'axios'
+import Project from './Project'
+import './Project.scss'
 
 function Portfolio() {
 
-  // if (this.props.data) {
-  //   var projects = this.props.data.projects.map(function (projects) {
-  //     var projectImage = 'images/portfolio/' + projects.image;
-  //     return <div key={projects.title} className="columns portfolio-item">
-  //       <div className="item-wrap">
-  //         <a href={projects.url} title={projects.title}>
-  //           <img alt={projects.title} src={projectImage} />
-  //           <div className="overlay">
-  //             <div className="portfolio-item-meta">
-  //               <h5>{projects.title}</h5>
-  //               <p>{projects.category}</p>
-  //             </div>
-  //           </div>
-  //           <div className="link-icon"><i className="fa fa-link"></i></div>
-  //         </a>
-  //       </div>
-  //     </div>
-  //   })
-  // }
-
   return (
     <section id="portfolio">
-
       <div className="row">
-
         <div className="twelve columns collapsed">
-
           <h1>Check Out Some of My Works.</h1>
+          <div id="portfolio-wrapper" className="project">
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+            <Project />
 
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-            {/* {projects} */}
           </div>
         </div>
       </div>
