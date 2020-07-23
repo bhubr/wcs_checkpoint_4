4ca592bfd0a90b61bab1bf7e2b38e2f889c0668a
LenaSim <radical.rin@gmail.com>
2020-07-24 01:30:27 +0200

    Add admin page

--- front/src/Components/Admin.js
+++ front/src/Components/Admin.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import ModalAdmin from './ModalAdmin'
+import ModalBack from './ModalBack'
+import './admin.scss'
 
 function Admin() {
+  const [showModal, toggleModal] = useState(false)
+  const openModal = () => {
+    toggleModal(true)
+  }
+
   return (
-    <div id='admin'>
+    <div className='admin'>
       <nav id="nav-wrap">
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
         <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
         <ul id="nav" className="nav">
-          <li className="current"><a className="smoothscroll" href="/">Home</a></li>
+          <li><a className="smoothscroll" href="/">Home</a></li>
           <li><a className="smoothscroll" href="/#about">About</a></li>
           <li><a className="smoothscroll" href="/#portfolio">Projects</a></li>
         </ul>
       </nav>
-      Admin
+      <button onClick={openModal}>Add a project</button>
+      {showModal && (
+        <>
+          <ModalAdmin toggleModal={toggleModal} />
+          <ModalBack toggleModal={toggleModal} />
+        </>
+      )}
     </div>
   )
 }
