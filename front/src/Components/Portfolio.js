03073385bb04442e6276e6ea0214dd694abd391b
LenaSim <radical.rin@gmail.com>
2020-07-23 13:41:14 +0200

    Add portfolio with cards

--- front/src/Components/Portfolio.js
+++ front/src/Components/Portfolio.js
@@ -1,27 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Project from './Project'
+import Modal from './Modal'
+import ModalBack from './ModalBack'
 import './Project.scss'
 
 function Portfolio() {
+  const [showModal, toggleModal] = useState(false)
+  const [modalId, setModalId] = useState()
+
+  const [project, setProject] = useState([])
+  const [techno, setTechno] = useState([])
+
+  // const projectContext = React.createContext(project)
+  // const technoContext = React.createContext(techno)
+
+  const openModal = (e) => {
+    console.log(e.target.id)
+    toggleModal(true)
+    setModalId(e.target.id)
+  }
+
+  const getPortfolio = () => {
+    const url = 'http://localhost:8080/project'
+    axios
+      .get(url)
+      .then(res => res.data)
+      .then(data => setProject(data))
+  }
+
+  const getTechno = () => {
+    const url = 'http://localhost:8080/techno'
+    axios
+      .get(url)
+      .then(res => res.data)
+      .then(data => setTechno(data))
+  }
+
+  useEffect(() => {
+    getPortfolio()
+    getTechno()
+  }, [])
 
   return (
-    <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="project">
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-
+    <>
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <div id="portfolio-wrapper" className="project">
+              {project.map(p =>
+                <Project key={p.id} project={p} techno={techno} openModal={openModal} />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {showModal && (
+        <>
+          <Modal
+            project={project.filter(p => p.project_id == modalId)}
+            techno={techno.filter(t => t.project_id == modalId)}
+            toggleModal={toggleModal}
+          />
+          <ModalBack toggleModal={toggleModal} />
+        </>
+      )}
+    </>
   )
 }
-
 export default Portfolio
--- front/src/Components/Project.js
+++ front/src/Components/Project.js
@@ -1,58 +1,17 @@
 import React from 'react'
 
-function Project(props) {
+function Project({ project, techno, openModal }) {
+  const url = `images/${project.src}`
   return (
-    <div class="post">
-      <div class="header_post">
-        <img src="https://images.pexels.com/photos/2529973/pexels-photo-2529973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+    <div className="project_post" id={project.project_id} onClick={(e) => openModal(e)}>
+      <div className="header_post" id={project.project_id} onClick={(e) => openModal(e)}>
+        <img src={url} alt="" id={project.project_id} onClick={(e) => openModal(e)} />
       </div>
-
-      <div class="body_post">
-        <div class="post_content">
-          <h1>Lorem Ipsum</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda cumque deserunt
-                    dolorum ex exercitationem.</p>
-          <div class="container_infos">
-            <div class="postedBy">
-              <span>author</span>
-                John Doe
-              </div>
-
-            <div class="container_tags">
-              <span>tags</span>
-              <div class="tags">
-                <ul>
-                  <li>design</li>
-                  <li>front end</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="body_post" id={project.project_id} onClick={(e) => openModal(e)}>
+        <h1 id={project.project_id} onClick={(e) => openModal(e)}>{project.title}</h1>
       </div>
     </div>
   )
 }
 
 export default Project
-
-
-  // if (this.props.data) {
-  //   var projects = this.props.data.projects.map(function (projects) {
-  //     var projectImage = 'images/portfolio/' + projects.image;
-  //     return <div key={projects.title} className="columns portfolio-item">
-  //       <div className="item-wrap">
-  //         <a href={projects.url} title={projects.title}>
-  //           <img alt={projects.title} src={projectImage} />
-  //           <div className="overlay">
-  //             <div className="portfolio-item-meta">
-  //               <h5>{projects.title}</h5>
-  //               <p>{projects.category}</p>
-  //             </div>
-  //           </div>
-  //           <div className="link-icon"><i className="fa fa-link"></i></div>
-  //         </a>
-  //       </div>
-  //     </div>
-  //   })
-  // }
