0d8d5a6680fb053f885f0af73e78ac83b7c8e791
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-08-10 04:20:00 +0200

    Increase form vertical space (scroll) and get criteria from server

--- front/src/Components/ModalAdmin.js
+++ front/src/Components/ModalAdmin.js
@@ -14,6 +14,7 @@ function ModalAdmin(props) {
   }
   const [newProject, setNewProject] = useState(initValue)
   const [newImg, setNewImg] = useState('')
+  const [allCriteria, setAllCriteria] = useState([])
 
   const handleInputChange = (e) => {
     setNewProject({
@@ -36,78 +37,88 @@ function ModalAdmin(props) {
     axios.post(urlProject, payload).then(() => props.toggleModal(false))
   }
 
+  const getCriteria = () => {
+    const url = '/criteria'
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .then((data) => setAllCriteria(data))
+  }
+
   return (
     <div className="modal_admin">
-      <div className="modal_admin_title">
-        <h1>Add a new project</h1>
+      <div className="modal_scroll">
+        <div className="modal_admin_title">
+          <h1>Add a new project</h1>
+        </div>
+        <form onSubmit={addNewProject}>
+          <div className="input">
+            <label>Project title</label>
+            <input
+              name="title"
+              className="form-field animation"
+              onChange={handleInputChange}
+              required
+            ></input>
+          </div>
+          <div className="input">
+            <label>Project description</label>
+            <textarea
+              name="description"
+              className="form-field animation"
+              onChange={handleInputChange}
+              required
+            ></textarea>
+          </div>
+          <div className="input">
+            <label>Created date</label>
+            <input
+              name="create_date"
+              type="date"
+              className="form-field animation"
+              onChange={handleInputChange}
+              required
+            ></input>
+          </div>
+          <div className="input">
+            <label>Site link</label>
+            <input
+              name="site_link"
+              className="form-field animation"
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="input">
+            <label>Github link</label>
+            <input
+              name="github_link"
+              className="form-field animation"
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="input">
+            <label>Image src</label>
+            <input
+              name="src"
+              placeholder="File name in /public/images"
+              className="form-field animation"
+              onChange={handleImgChange}
+              required
+            ></input>
+          </div>
+          <div className="input">
+            <label>Company Name</label>
+            <input
+              name="company_name"
+              className="form-field animation"
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
-      <form onSubmit={addNewProject}>
-        <div className="input">
-          <label>Project title</label>
-          <input
-            name="title"
-            className="form-field animation"
-            onChange={handleInputChange}
-            required
-          ></input>
-        </div>
-        <div className="input">
-          <label>Project description</label>
-          <textarea
-            name="description"
-            className="form-field animation"
-            onChange={handleInputChange}
-            required
-          ></textarea>
-        </div>
-        <div className="input">
-          <label>Created date</label>
-          <input
-            name="create_date"
-            type="date"
-            className="form-field animation"
-            onChange={handleInputChange}
-            required
-          ></input>
-        </div>
-        <div className="input">
-          <label>Site link</label>
-          <input
-            name="site_link"
-            className="form-field animation"
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div className="input">
-          <label>Github link</label>
-          <input
-            name="github_link"
-            className="form-field animation"
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div className="input">
-          <label>Image src</label>
-          <input
-            name="src"
-            placeholder="File name in /public/images"
-            className="form-field animation"
-            onChange={handleImgChange}
-            required
-          ></input>
-        </div>
-        <div className="input">
-          <label>Company Name</label>
-          <input
-            name="company_name"
-            className="form-field animation"
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div className="center">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
     </div>
   )
 }
